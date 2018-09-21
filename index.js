3a9a1eaddd224e0a4ea5bc4e82a96ee361ad649e
P S <s.prerita@gmail.com>
2018-09-21 17:57:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ function destructivelyRemoveFirstKitten() {kittens.shift(); return kittens}
 function appendKitten(name) {return [...kittens, "Broom"]}
 function prependKitten(name) {return ["Arnold",...kittens]}
 function removeLastKitten() {return kittens.slice(0, kittens.length -1)}
-function removeFirstKitten() {const kit=kittens[0]return kittens.slice(1);}
+function removeFirstKitten() {const kit=kittens[0];return kittens.slice(1);}
